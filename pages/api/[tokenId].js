41016b9107c4e6e34886a228c564b5a148446619
DuyVo96 <vominhduy.cokhi@gmail.com>
2023-04-19 16:26:40 +0700

    change

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -1,16 +1,12 @@
 export default function handler(req, res) {
-  // get the tokenId from the query params
   const tokenId = req.query.tokenId;
-  // As all the images are uploaded on github, we can extract the images from github directly.
   const image_url =
     "https://raw.githubusercontent.com/DuyVo96/NFT-Celestia/main/public/CelestiaNft/";
-  // The api is sending back metadata for a Crypto Dev
-  // To make our collection compatible with Opensea, we need to follow some Metadata standards
-  // when sending back the response from the api
-  // More info can be found here: https://docs.opensea.io/docs/metadata-standards
+
   res.status(200).json({
-    name: "Crypto Dev #" + tokenId,
-    description: "Crypto Dev is a collection of developers in crypto",
+    name: "Celestia NFT #" + tokenId,
+    description:
+      "Celestia NFT is a collection for all node runners of Celestia Network",
     image: image_url + tokenId + ".svg",
   });
 }
