c4853ac9c7111d6f619288f73d39d78e0d73b16d
DuyVo96 <vominhduy.cokhi@gmail.com>
2023-04-20 17:59:36 +0700

    change metadata

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   const image_url =
-    "https://raw.githubusercontent.com/DuyVo96/NFT-Celestia/main/public/CelestiaNft/";
+    "https://raw.githubusercontent.com/DuyVo96/CELESTIA-NFT-COLLECTION/main/public/CelestiaNft/";
 
   res.status(200).json({
     name: "Celestia NFT #" + tokenId,
